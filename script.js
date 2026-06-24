f1b16be36d5273721e10114fedd2824be03bd0b4
afasilvarj <71022822+afasilvarj@users.noreply.github.com>
2026-06-24 16:22:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function mostrarTela(id) {
+  const telas = document.querySelectorAll('.tela');
 
+  telas.forEach(tela => {
+    tela.classList.remove('ativa');
+  });
+
+  document.getElementById(id).classList.add('ativa');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  mostrarTela('dashboard');
+});
